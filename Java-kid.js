b61d5f3a1ae4be9f72d1f0f9769390c3188ff0ba
DonaldMzima <donaldmzima8@gmail.com>
2022-05-07 03:37:46 +0200

    adding circles

--- Java-kid.js
+++ Java-kid.js
@@ -1,7 +1,1 @@
 
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
-
-navToggle.addEventListener('click', function(){
-    links.classList.toggle("show_nav");
-})
